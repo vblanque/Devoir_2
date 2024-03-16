d5f15bc419d3d564621325246d2378e50bd92e83
Victor Blanquez <victor.blanquez-yeste@polymtl.ca>
2024-03-16 17:49:25 -0400

    First solution without local maxima exit

--- RechercheLocale/solver.py
+++ RechercheLocale/solver.py
@@ -20,13 +20,20 @@ def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
         La premiere valeur est une liste représentant les stations principales ouvertes au format [0, 1, 0] qui indique que seule la station 1 est ouverte
         La seconde valeur est une liste représentant les associations des stations satellites au format [1 , 4] qui indique que la premiere station est associée à la station pricipale d'indice 1 et la deuxieme à celle d'indice 4
     """
+    # initial solution and cost
     init = initial_solution(problem)
     sol = init
-    cost = problem.calculate_cost(sol[0],sol[1])
-    neighbours = create_neighbours(problem, sol)
-    print(sol)
-    print(cost) 
-    print(neighbours)
+    best_cost = problem.calculate_cost(sol[0],sol[1])
+    # 
+    for i in range(10):
+        # explore neighbour solutions
+        neighbours = create_neighbours(problem, sol)
+        # select solution with lowest cost
+        for neigh in neighbours:
+            cost = problem.calculate_cost(neigh[0],neigh[1])
+            if cost < best_cost:
+                sol = neigh
+                best_cost = cost
     return sol
 
 
@@ -39,19 +46,45 @@ def initial_solution(problem: UFLP):
     return main_stations.tolist(), satellites.tolist()
 
 def create_neighbours(problem: UFLP, sol):
-    """Generate other solution by changing the main stations.
-    Add or remove a station if it's possible.
-    Assign the satellite to the nearest main station"""
-    main_stations = sol[0]
+    """Generate other solutions by changing the number of main stations.
+    Adding or removing one main station if it's possible.
+    Assign the satellite stations to their nearest main station"""
+    current_mains = sol[0]
     neighbours = []
-    # Solution with an additional station
-    if main_stations.count(1) < problem.n_main_station : #check if all stations are already opened
+    # Solutions with an additional main station
+    if current_mains.count(1) < problem.n_main_station : #check if all main stations are already opened
         for i in range(problem.n_main_station):
-            if main_stations[i] == 0: #check if station is already opened
-                new_mains = main_stations.copy()
-                new_mains[i] +=1
-                neighbours.append(new_mains)
+            if current_mains[i] == 0: #check if station is not opened
+                new_mains = current_mains.copy()
+                new_mains[i] = 1
+                satellites = assign_nearest_mains(problem, new_mains)      
+                neighbours.append((new_mains, satellites))
+    # Solutions with one less main station
+    if current_mains.count(1) > 1 : #check if only one main station is opened
+        for i in range(problem.n_main_station):
+            if current_mains[i] == 1: #check if station is opened
+                new_mains = current_mains.copy()
+                new_mains[i] = 0
+                satellites = assign_nearest_mains(problem, new_mains)      
+                neighbours.append((new_mains, satellites))
     return neighbours
-    
-    
-    
+
+def assign_nearest_mains(problem, main_stations):
+    """Assign sattelites to the nearest main station"""
+    satellites = []
+    # for all sattelites stations
+    for sat_coord in problem.satellite_stations_connection_coordinates:
+        min_distance = float('inf')
+        closest_main = None
+        i = 0
+        # for all main stations
+        for main_coord in problem.main_stations_coordinates:
+            if main_stations[i] == 1: # check if main station is opened
+                # calculate distance and keep the nearest main station 
+                distance = problem.coordinates_to_cost(sat_coord[0],sat_coord[1],main_coord[0],main_coord[1]) 
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_main = i
+            i += 1
+        satellites.append(closest_main)
+    return satellites
