2b4966f7d3cc701c20385132453dc38d0731436b
Victor Blanquez <victor.blanquez-yeste@polymtl.ca>
2024-03-21 18:50:44 -0400

    Recherche Locale avec restarts

--- RechercheLocale/solver.py
+++ RechercheLocale/solver.py
@@ -3,8 +3,15 @@ from typing import List, Tuple
 import numpy as np
 """ 
     Binome 1 : Blanquez Victor (2225992)
-    Binome 2 : Nom Prenom (Matricule)
-    Description succinte de l'implementation :
+    Binome 2 : Dietrich Colin (2226611)
+    Description succinte de l'implementation : Nous avons implementé un recherche locale avec des restarts. 
+    Le voisinage de chaque solution est l'ensemble des solutions possibles ayant une gare principale en plus
+    ou en moins que la solution actuelle. Pour toutes les solutions considérées, les stations sattelites sont
+    assignées à la station principale la plus proche. Ce voisinage est connecté mais peut comporter des minimas
+    locaux. Une première recherche à l'aide d'une solution initiale optimisée est effectuée, sa solution peut
+    être une minima local.
+    Pour pouvoir sortir de ces minimas et introduire de la diversification, des recherches locales 
+    à partir de solution initiales aléatoires sont effectuées. 
     ...
 """
 
@@ -20,12 +27,31 @@ def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
         La premiere valeur est une liste représentant les stations principales ouvertes au format [0, 1, 0] qui indique que seule la station 1 est ouverte
         La seconde valeur est une liste représentant les associations des stations satellites au format [1 , 4] qui indique que la premiere station est associée à la station pricipale d'indice 1 et la deuxieme à celle d'indice 4
     """
+    # first local search with forced intial solution (beats secret agent)
+    sol = local_search_init(problem, 10)
+    cost = problem.calculate_cost(sol[0],sol[1])
+    # n local search with random initial solution (diversification)
+    for i in range(20):
+        # new solution with local search
+        new_sol = local_search_random(problem, 100)
+        new_cost = problem.calculate_cost(new_sol[0],new_sol[1])
+        # select solution with lowest cost
+        if new_cost<cost:
+            sol = new_sol
+            cost = new_cost
+            print("Search with a random initial solution found better solution")
+    return sol
+
+def local_search_init(problem: UFLP, n):
+    """Local search with a forced initial solution : 
+    Only the cheapest main station is opened. 
+    The neighbours are solution with one more (or one less) main station"""
     # initial solution and cost
     init = initial_solution(problem)
     sol = init
     best_cost = problem.calculate_cost(sol[0],sol[1])
-    # 
-    for i in range(10):
+    # n-search in neighbours and selection
+    for i in range(n):
         # explore neighbour solutions
         neighbours = create_neighbours(problem, sol)
         # select solution with lowest cost
@@ -36,14 +62,43 @@ def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
                 best_cost = cost
     return sol
 
+def local_search_random(problem: UFLP, n):
+    """Local search with a random initial solution : 
+    Main stations are opened randomly.
+    The neighbours are solution with one more (or one less) main station"""
+    # initial solution and cost
+    init = random_solution(problem)
+    sol = init
+    best_cost = problem.calculate_cost(sol[0],sol[1])
+    # n-search in neighbours and selection
+    for i in range(n):
+        # explore neighbour solutions
+        neighbours = create_neighbours(problem, sol)
+        # select solution with lowest cost
+        for neigh in neighbours:
+            cost = problem.calculate_cost(neigh[0],neigh[1])
+            if cost < best_cost:
+                sol = neigh
+                best_cost = cost
+    return sol
 
 def initial_solution(problem: UFLP):
-    """Opening the main station with cheapest opening cost"""
+    """Creation of a specific initital solution :
+    Opening only one main station with cheapest opening cost"""
     main_stations = np.zeros(problem.n_main_station, dtype=np.int32) #no opened main station 
     cheap = np.argmin(problem.main_stations_opening_cost)
     main_stations[cheap] = 1 #opening the cheapest one
     satellites = np.zeros(problem.n_satellite_station, dtype=np.int32) + cheap # associate sattelites with cheapest main station
     return main_stations.tolist(), satellites.tolist()
+
+def random_solution(problem: UFLP):
+    """Creation of a random solution :
+    Opening random main stations to indroduce diversification.
+    Assign the satellite stations to their nearest main station."""
+    # Create a random array of 4 digits (0 or 1)
+    main_stations = np.random.randint(2, size=problem.n_main_station, dtype=np.int32)
+    satellites = assign_nearest_mains(problem, main_stations)
+    return main_stations.tolist(), satellites
 
 def create_neighbours(problem: UFLP, sol):
     """Generate other solutions by changing the number of main stations.
